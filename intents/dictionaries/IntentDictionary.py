387ebc663616e99a8fbfe885831710c574d30310
Gordon <just8574322589@gmail.com>
2022-06-21 22:25:46 +0800

    [Gordon][Update] 新增新的意圖到字典

--- intents/dictionaries/IntentDictionary.py
+++ intents/dictionaries/IntentDictionary.py
@@ -4,5 +4,9 @@ patterns = {
     re.compile('^[雷達|天氣].*'): ['actions.ActionWeatherInfo', 'get_cloud_image'],
     re.compile('.*[搜尋|搜|找|點播]歌手(.*)'): ['actions.ActionSearchSongsInKKBox', 'search_singer'],
     re.compile('.*[搜尋|搜|找](.*)的專輯'): ['actions.ActionSearchSongsInKKBox', 'search_singer_album'],
-    re.compile('.*[搜尋|搜|找](.*)的[歌曲|歌]'): ['actions.ActionSearchSongsInKKBox', 'search_singer_song']
+    re.compile('.*[搜尋|搜|找](.*)的[歌曲|歌]'): ['actions.ActionSearchSongsInKKBox', 'search_singer_song'],
+    re.compile('.*[搜尋|搜|找]專輯(.*)'): ['actions.ActionSearchSongsInKKBox', 'search_possible_albums'],
+    re.compile('.*[搜尋|搜|找][歌曲|歌](.*)'): ['actions.ActionSearchSongsInKKBox', 'search_possible_songs'],
+    re.compile('.*流行音樂.*'): ['actions.ActionSearchSongsInKKBox', 'list_fever_playlists'],
+    re.compile('.*排行榜.*'): ['actions.ActionSearchSongsInKKBox', 'view_charts']
 }
